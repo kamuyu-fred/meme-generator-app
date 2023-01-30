f0d2f6f07a70a0fd64e770e163fd02c8dd19dfa6
fred <daktarispacetime@gmail.com>
2023-01-30 08:07:57 +0300

    initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Home.css";
+
+
+function Home() {
+    return (
+        <div>
+            
+            <p>WELCOME TO MEME GENERATOR</p>
+
+        </div>
+)
+}
+
+
+export default Home;
