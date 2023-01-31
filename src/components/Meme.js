21dffe25ce365e74c72f93e16a31d9d6209bab99
kipchumba-ket <kipchumbaelly97@gmail.com>
2023-01-31 17:33:58 +0300

    first fix

---
+++ src/components/Meme.js
@@ -0,0 +1,34 @@
+import React, {createRef} from 'react'
+import { exportComponentAsJPEG } from 'react-component-export-image';
+
+function Meme({imageSrc, inputs}) {
+    const componentRef = createRef()
+
+  /* const params = {
+     html2CanvasOptions: {
+          backgroundColor: null,
+          scrollX: -window.scrollX,
+       scrollY: -window.scrollY,
+          windowWidth: document.documentElement.offsetWidth,
+         windowHeight: document.documentElement.offsetHeight8 
+     }*/
+
+  
+    return (
+      <div className="meme__holder">
+        <div id="meme" className="meme" ref={componentRef}> 
+          {<img className="meme__image" src={imageSrc} alt="meme" width="100" />}
+          <h2 className="top">{inputs.toptext}</h2>
+          <h2 className="bottom">{inputs.bottomtext}</h2>
+        </div>
+        <div className="meme__exporter">
+        <button className="btn" onClick={() => exportComponentAsJPEG(componentRef)}>
+                Save as JPEG
+        </button>
+        
+            </div>
+        </div>
+    )
+  }
+
+  export default Meme;
