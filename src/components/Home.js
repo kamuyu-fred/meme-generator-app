e38aba0cdcc0c51560a0af66b04c796a9ef91458
kipchumba-ket <kipchumbaelly97@gmail.com>
2023-02-01 11:19:20 +0300

    css to homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Home.css";
-
 
 function Home() {
     return (
-        <div>
+        <div style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            height: "100vh"
+          }}>
             
             <span><h1>WELCOME TO MEME GENERATOR</h1></span>
-
-              
-                    <h2>A quick and easy way to generate memes</h2>
-               
-        </div>
-)
+          
+            <h2>A quick and easy way to generate memes</h2>
+            
+            <Link to="/generator">
+              <button>Generator</button>
+            </Link>
+          
+          </div>
+          
+    );
 }
-
 
 export default Home;
