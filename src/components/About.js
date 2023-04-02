208dee5cc5865b13b43fa929b5730f65852d1145
kamuyu-f9880 <117721227+kamuyu-fred@users.noreply.github.com>
2023-04-02 22:06:32 +0300

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -7,8 +7,7 @@ function About() {
         <h1>About</h1>
 
         <div className="about-div">
-        This is a meme generator site designed for quick, fast and easy meme creation. It is designed to be minimal and easy to use while having very low overhead to maximise responsiveness. This is the phase 2 group project for the team comprising of the following member:
-            
+        This is a meme generator site designed for quick, fast and easy meme creation.   
               
               <li>
                 Edgar Ondara
