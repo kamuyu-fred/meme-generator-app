70a44a0d0d6f6eab3ee01a77321e2c25e97043ef
kamuyu-f9880 <117721227+kamuyu-fred@users.noreply.github.com>
2023-04-02 22:05:27 +0300

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -7,20 +7,11 @@ function About() {
         <h1>About</h1>
 
         <div className="about-div">
-        This is a meme generator site designed for quick, fast and easy meme creation. It is designed to be minimal and easy to use while having very low overhead to maximise responsiveness. This is the phase 2 group project for the team comprising of the following members:
+        This is a meme generator site designed for quick, fast and easy meme creation. It is designed to be minimal and easy to use while having very low overhead to maximise responsiveness. This is the phase 2 group project for the team comprising of the following member:
             
               
               <li>
-                John Justice
-              </li>
-              <li>
-                Esperance Kariuki
-              </li>
-              <li>
-                Elly Kipchumba
-              </li>
-              <li>
-                Fred Kamuyu
+                Edgar Ondara
               </li>
               
         </div>
